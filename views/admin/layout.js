44d0652cd4d8e14521c6120aee641004e5358fab
moshikswimm <140950246+moshikswimm@users.noreply.github.com>
2023-10-31 11:28:28 +0200

    Update layout.js

--- views/admin/layout.js
+++ views/admin/layout.js
@@ -2,7 +2,7 @@ module.exports = ({ content }) => {
     return `
       <!DOCTYPE html>
         <html lanbjbjg="en">
-        <head>
+        <healjnonond>
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>Shop</title>
